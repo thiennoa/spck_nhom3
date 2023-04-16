1af5e9922caa5a63eb1e8d0d1ef4d8f5498fd13b
thiennoa <thientvt11@gmail.com>
2023-04-16 21:49:18 +0700

    text

--- login.js
+++ login.js
@@ -12,106 +12,87 @@
         });
 
 
+// Lấy các phần tử DOM từ HTML
 
-// đăng nhập hiện logo user
+const registerButton = document.getElementById('click-register');
+const loginButton = document.getElementById('click-login');
 
+// Đăng ký sự kiện click cho nút "Đăng ký" trên form đăng ký
+registerButton.addEventListener('click', () => {
+    const username = document.getElementById('user-name').value;
+    const password = document.getElementById('pass').value;
+    const endPassword = document.getElementById('end-pass').value;
+    
+    // Kiểm tra xem mật khẩu đã được nhập lại đúng chưa
+    if (password !== endPassword) {
+        alert('Mật khẩu không khớp');
+        return;
+    }
 
-const $clickLogin = document.getElementById('click-login');
-const $clickRegister = document.getElementById('click-register');
-const $boxMain = document.getElementById('box-main');
-const $boxMainLogin = document.getElementById('box-main-login');
-function checkPasswordStrength(password) {
-    // Initialize variables
-    var strength = 0;
-    var tips = "";
   
-    // Check password length
-    if (password.length < 8) {
-      tips += "Make the password longer. ";
-    } else {
-      strength += 1;
+    // Kiểm tra xem các trường thông tin có được điền đầy đủ hay không
+    if (username === '' || password === '' || endPassword === '') {
+        alert('Vui lòng điền đầy đủ thông tin!');
+        return;
     }
-  
-    // Check for mixed case
-    if (password.match(/[a-z]/) && password.match(/[A-Z]/)) {
-      strength += 1;
-    } else {
-      tips += "Use both lowercase and uppercase letters. ";
-    }
-  
-    // Check for numbers
-    if (password.match(/\d/)) {
-      strength += 1;
-    } else {
-      tips += "Include at least one number. ";
-    }
-  
-    // Check for special characters
-    if (password.match(/[^a-zA-Z\d]/)) {
-      strength += 1;
-    } else {
-      tips += "Include at least one special character. ";
-    }
-  
-    // Return results
-    if (strength < 2) {
-      return "Easy to guess. " + tips;
-    } else if (strength === 2) {
-      return "Medium difficulty. " + tips;
-    } else if (strength === 3) {
-      return "Difficult. " + tips;
-    } else {
-      return "Extremely difficult. " + tips;
-    }
+
+    // Kiểm tra xem mật khẩu và xác nhận mật khẩu có khớp nhau hay không
+    if (password !== endPassword) {
+        alert('Mật khẩu và xác nhận mật khẩu không khớp!');
+        return;
   }
 
+    // Kiểm tra xem người dùng đã tồn tại trong mảng người dùng chưa
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+      alert('Người dùng đã tồn tại!');
+      return;
+  }
 
+  // Thêm người dùng mới vào mảng người dùng và lưu vào Local Storage
+  const newUser = { username, password };
+  users.push(newUser);
+  localStorage.setItem('users', JSON.stringify(users));
 
-let users = []
-if(localStorage.getItem('users')){
-    users = JSON.stringify(localStorage.getItem('users'))
-} 
-
-const handleRegister = (e) => {
-    e.preventDefault();
-    const $userName = document.getElementById('user-name').value;
-    const $pass = document.getElementById('pass').value;
-    const $endPass = document.getElementById('end-pass').value;
-    if($pass.lenght < 6){
-        alert('Độ dài mật khẩu khồng đủ');
-    }
-
-
-    if($pass != $endPass){
-        alert('mật khẩu không trùng khớp');
-    } else{
-        const user = {
-            id: Date.now(),
-            username: $userName,
-            pass: $pass
-        }
-        users.push(user);
-        localStorage.setItem('users', JSON.stringify(users));
-        window.location.href = 'http://127.0.0.1:5500/index.html';// chỉnh http
-    }
-}
-
-const handleLogin = (e) => {
-    e.preventDefault();
-    const $userName = document.getElementById('user-name').value;
-    const $pass = document.getElementById('pass').value;
-    const user = JSON.parse(localStorage.getItem('user'));
-    if($userName == user.username && $pass == user.pass){
-        window.location.href = 'http://127.0.0.1:5500/header.html';
-        localStorage.setItem('user_login', user.username );
-        // const user = localStorage.getItem('user')
-
-    }
+  // Hiển thị thông báo đăng kí thành công và chuyển sang trang đăng nhập
+    alert('Đăng kí thành công!');
+    signInButton.click();
     
     
-}
+//     // Tạo đối tượng user và lưu vào Local Storage
+//     const user = { username, password };
+//     localStorage.setItem('user', JSON.stringify(user));
+});
+
+// Đăng ký sự kiện click cho nút "Đăng nhập" trên form đăng nhập
+loginButton.addEventListener('click', () => {
+   let isLoginSuccess = false;
+   
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    // Kiểm tra xác thực thông tin đăng nhập
+    const listUser = JSON.parse(localStorage.getItem('users'));
+
+    for (let i = 0; i < listUser.length; i++) 
+      {
+          if(listUser[i].username === email && listUser[i].password === password){
+            isLoginSuccess=true;
+          }
+      }
+  
+    
+   if(isLoginSuccess===true){
+    window.location.assign("http://127.0.0.1:5500/header.html")
+   } else {
+    alert("Tên đăng nhập hoặc mật khẩu không đúng.")
+   }
+
+   localStorage.setItem("isLoginSuccess", isLoginSuccess)
+  })
 
 
 
-$boxMain?.addEventListener('submit', handleRegister);
-$boxMainLogin.addEventListener('submit', handleLogin);
+
+
